1c6829679334555b8fe061604f10e5036cb72a77
Michal Dabrowski <dabrowski@brillante.pl>
2017-05-20 14:57:26 +0200

    Chaper 8 exercises

---
+++ hfp/chapter8/numbers_to_words.hs
@@ -0,0 +1,21 @@
+module WordNumber where 
+import Data.List (intersperse)
+
+digitToWord :: Int -> String 
+digitToWord 0 = "zero"
+digitToWord 1 = "one"
+digitToWord 2 = "two"
+digitToWord 3 = "three"
+digitToWord 4 = "four"
+digitToWord 5 = "five"
+digitToWord 6 = "six"
+digitToWord 7 = "seven"
+digitToWord 8 = "eight"
+digitToWord 9 = "nine"
+
+digits :: Int -> [Int] 
+digits 0 = []
+digits x = digits (div x 10) ++ [mod x 10]
+
+wordNumber :: Int -> String 
+wordNumber n = concat $ intersperse "-" (map digitToWord (digits n)) 
